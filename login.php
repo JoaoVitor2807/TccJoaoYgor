582b7c65076f3aa05c4d06fb45655426aa0d4cb8
JoaoVitor2807 <joaovgp8147@gmail.com>
2024-08-20 09:01:44 -0300

    php

--- login.php
+++ login.php
@@ -2,19 +2,35 @@
     session_start();
     include 'db_connection.php';
 
-    if($_SERVER["REQUEST METHOD"] == "POST") {
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
         $sql = "SELECT id, password FROM users WHERE username = ?";
-        if($stmt = @conn->prepare($sql)) {
+        if($stmt = $conn->prepare($sql)) {
             $stmt->bind_param ("s", $username);
             $stmt->execute();
             $stmt->store_result();
 
             if($stmt->num_rows == 1) {
-                $stmt->bind_result($id,$hashed_passord)
+                $stmt->bind_result($id,$hashed_passord);
+                $stmt->fetch();
+
+                if (password_verify($password, $hashed_passord)) {
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['id'] = $id;
+                    $_SESSION['username'] = $username;
+
+                    header("location: index.html");
+                } else {
+                    echo "senha incorreta.";
+                }
+            } else {
+                echo "Nenhum usuário encontrado com esse username.";
             }
+            $stmt->close();
         }
     }
+
+$conn->close();
 ?>
